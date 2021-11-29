0df4186a2837efb110281e6b864c70be7ce62fb8
JavanAppleby <styx_n_stonz@hotmail.com>
2021-11-29 18:11:25 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.body.innerHTML = `<h1 id="victory"></h1>`
+
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "Javan is the champion";
